3b268bcaadc136ecee9d9cddd67c3ae9dde6d229
garrettomi <omigarrett@gmail.com>
2023-05-27 18:12:27 +0900

    Fix: Testing login endpoint for deployment

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,7 +11,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${process.env.API_URL}/login`, {
+      const response = await axios.post(process.env.API_URL + "/login", {
         username,
         password,
       });

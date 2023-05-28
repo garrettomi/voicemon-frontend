52f0ce6715387e49538fb092df29818d763e8f5a
garrettomi <omigarrett@gmail.com>
2023-05-28 20:30:53 +0900

    Repositioning image now that it has loaded

--- src/pages/login.js
+++ src/pages/login.js
@@ -83,7 +83,7 @@ export default function Login() {
             <Link href="/signup">Sign Up</Link>
           </div>
         </div>
-        <div className="loginPageBackground">
+        <div className={styles.loginPageBackground}>
           <Image src={loginimg} alt="Login background" />
         </div>
       </div>
